b75db55eaf05ca04b649528de22ac3df34354ffd
clepz <tarik_41.41@hotmail.com>
2019-11-06 09:46:20 +0300

    changes

--- selfdrive/visiond/models/commonmodel.c
+++ selfdrive/visiond/models/commonmodel.c
@@ -43,12 +43,42 @@ float *model_input_prepare(ModelInput* s, cl_command_queue q,
                   s->transformed_y_cl, s->transformed_u_cl, s->transformed_v_cl,
                   s->transformed_width, s->transformed_height,
                   transform);
+
+
+  //----------------
+  float *yuv_cl_y = (float *)clEnqueueMapBuffer(q, s->transformed_y_cl, CL_TRUE,
+                                            CL_MAP_READ, 0, width*height,
+                                            0, NULL, NULL, &err);
+  FILE *yuv_cl_y_file = fopen("/sdcard/yuv_cl_y.y", "wb");
+    fwrite(yuv_cl_y, width*height, sizeof(float), yuv_cl_y_file);
+    fclose(yuv_cl_y_file);
+
+  //----------------
+  float *yuv_cl_u = (float *)clEnqueueMapBuffer(q, s->transformed_u_cl, CL_TRUE,
+                                            CL_MAP_READ, 0, width*height/4,
+                                            0, NULL, NULL, &err);
+  FILE *yuv_cl_u_file = fopen("/sdcard/yuv_cl_u.u", "wb");
+    fwrite(yuv_cl_u, width*height/4, sizeof(float), yuv_cl_u_file);
+    fclose(yuv_cl_u_file);
+
+  //----------------
+  float *yuv_cl_v = (float *)clEnqueueMapBuffer(q, s->transformed_v_cl, CL_TRUE,
+                                            CL_MAP_READ, 0, width*height/4,
+                                            0, NULL, NULL, &err);
+  FILE *yuv_cl_v_file = fopen("/sdcard/yuv_cl_v.v", "wb");
+    fwrite(yuv_cl_v, width*height/4, sizeof(float), yuv_cl_v_file);
+    fclose(yuv_cl_v_file);
+  //----------------
   loadyuv_queue(&s->loadyuv, q,
                 s->transformed_y_cl, s->transformed_u_cl, s->transformed_v_cl,
                 s->net_input);
   float *net_input_buf = (float *)clEnqueueMapBuffer(q, s->net_input, CL_TRUE,
                                             CL_MAP_READ, 0, s->net_input_size,
                                             0, NULL, NULL, &err);
+  printf("commonmodel s->net_input_size: %d\n", s->net_input_size);
+
+
+
   clFinish(q);
   return net_input_buf;
 }
